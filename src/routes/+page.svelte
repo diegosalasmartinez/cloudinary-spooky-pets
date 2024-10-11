dfe0ec1ed0a5026947e7e1444faaad6b43be7592
Diego <diesalasmart@gmail.com>
2024-10-10 19:58:42 -0500

    Add fonts and colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,40 @@
-<h1>Welcome to Cloudinary Spooky Pets</h1>
+<h1>Cloudinary Spooky Pets</h1>
+<section>
+	<div>
+		<h2>Give your pet a <span>spooky transformation!</span></h2>
+		<p>
+			Upload a photo of your pet and watch them transform into a spooky creature! Whether it's
+			turning your cat into a ghost or your dog into a pumpkin, we've made it simple, fun, and
+			accessible for everyone.
+		</p>
+	</div>
+</section>
+
+<style>
+	h1,
+	h2 {
+		color: var(--primary-color);
+	}
+
+	section {
+		display: grid;
+		grid-template-columns: 3fr 2fr;
+		gap: 20px;
+		margin-top: 40px;
+	}
+
+	section h2 {
+		font-size: 72px;
+	}
+
+	section h2 span {
+		font-family: 'HalloweenMorning', sans-serif;
+	}
+
+	section p {
+		color: white;
+		font-size: 22px;
+		line-height: 30px;
+		margin-top: 10px;
+	}
+</style>
