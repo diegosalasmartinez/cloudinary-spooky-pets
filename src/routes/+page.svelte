7daf3ad08d4dc1003a95b935fee8ffa58c2fa784
Diego <diesalasmart@gmail.com>
2024-10-20 20:00:55 -0500

    fix: update h1 tag, responsive breakpoints and add help text under the button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,8 +16,8 @@
 </script>
 
 <section>
-	<h1>Give your pet a</h1>
-	<h2 class="spooky">spooky transformation!</h2>
+	<h2>Give your pet a</h2>
+	<h1 class="spooky">spooky transformation!</h1>
 	<p>
 		Upload a photo of your pet and watch them transform into a spooky creature! Whether it's turning
 		your cat into a ghost or your dog into a pumpkin, we've made it simple, fun, and accessible for
@@ -35,21 +35,25 @@
 			onUpload={handleUpload}
 		>
 			<button on:click={() => open()} disabled={isLoading}>Upload a photo</button>
+			<span
+				>For better results, upload a clear image with a light background, and ensure the file size
+				is less than 100MB.</span
+			>
 		</CldUploadWidget>
 	</div>
 </section>
 
 <style>
 	h1 {
-		margin-top: 40px;
-		margin-bottom: 10px;
+		font-family: 'HalloweenMorning', sans-serif;
 		font-size: 40px;
+		line-height: 40px;
 	}
 
 	h2 {
-		font-family: 'HalloweenMorning', sans-serif;
-		font-size: 50px;
-		line-height: 50px;
+		margin-top: 40px;
+		margin-bottom: 10px;
+		font-size: 30px;
 	}
 
 	p {
@@ -61,36 +65,48 @@
 		text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.8);
 	}
 
-	@media (min-width: 440px) {
+	@media (min-width: 360px) {
 		h1 {
+			color: red;
 			font-size: 50px;
+			line-height: 50px;
 		}
 
 		h2 {
+			font-size: 40px;
+		}
+	}
+
+	@media (min-width: 440px) {
+		h1 {
 			font-size: 60px;
 			line-height: 60px;
+		}
+
+		h2 {
+			font-size: 50px;
 		}
 	}
 
 	@media (min-width: 640px) {
 		h1 {
-			font-size: 65px;
+			font-size: 80px;
+			line-height: 80px;
 		}
 
 		h2 {
-			font-size: 80px;
-			line-height: 80px;
+			font-size: 65px;
 		}
 	}
 
 	@media (min-width: 768px) {
 		h1 {
-			font-size: 72px;
+			font-size: 100px;
+			line-height: 100px;
 		}
 
 		h2 {
-			font-size: 100px;
-			line-height: 100px;
+			font-size: 72px;
 		}
 
 		p {
@@ -101,7 +117,9 @@
 
 	#button-container {
 		display: flex;
-		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		gap: 15px;
 		margin-top: 50px;
 	}
 
@@ -120,4 +138,10 @@
 	button:hover {
 		transform: scale(1.1);
 	}
+
+	#button-container span {
+        text-align: center;
+        font-size: 14px;
+        color: #eee;
+    }
 </style>
